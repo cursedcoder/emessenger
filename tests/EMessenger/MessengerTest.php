16230158fc7227c618874bc4361b124bb54296a1
Yevhenii Huselietov <d46k16@gmail.com>
2016-04-18 15:41:06 +0800

    Red test impossible to send more than 1 message at the same time

--- tests/EMessenger/MessengerTest.php
+++ tests/EMessenger/MessengerTest.php
@@ -19,10 +19,10 @@ class MessengerTest extends \PHPUnit_Framework_TestCase
         $loop = LoopFactory::create();
         $transport = new UnixTransport($loop, 'unix://' . $this->getUnixPath());
 
-        $this->transport_works($loop, $transport);
+        $this->transport_works_simulatenously($loop, $transport);
     }
 
-    private function transport_works(LoopInterface $loop, TransportInterface $transport)
+    private function transport_works_simulatenously(LoopInterface $loop, TransportInterface $transport)
     {
         $server = MessengerFactory::server($transport);
         $client = MessengerFactory::client($transport);
@@ -30,29 +30,10 @@ class MessengerTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Messenger::class, $server);
         $this->assertInstanceOf(Messenger::class, $client);
 
-        $event = 'test';
-        $eventArg = 'test arg';
-        $isCalled = false;
-
-        $client->on($event, function($arg) use ($eventArg, &$isCalled, $loop) {
-            $this->assertEquals($arg, $eventArg);
-            $isCalled = true;
-            $loop->stop();
-        });
-
-        $loop->addTimer(0.001, function() use ($server, $event, $eventArg) {
-            $server->send($event, $eventArg);
-        });
-
-        $loop->addTimer(3, function() use ($loop) {
-            $loop->stop();
-
-            throw new \RuntimeException('Timeout reached, server did not run.');
-        });
-
-        $loop->run();
-
-        $this->assertTrue($isCalled);
+        $tester = new Tester();
+        $tester->messages[] = new TestMessage('event1', 'args1');
+        $tester->messages[] = new TestMessage('event2', 'args123');
+        $tester->initialize($server, $client, $loop);
     }
 
     public function tearDown()
@@ -65,5 +46,55 @@ class MessengerTest extends \PHPUnit_Framework_TestCase
     public function getUnixPath()
     {
         return realpath(__DIR__ . '/../../') . '/tmp/test.sock';
+    }
+}
+
+class Tester extends \PHPUnit_Framework_TestCase
+{
+    public $messages = [];
+    public $totalCalled = 0;
+
+    public function initialize(Messenger $server, Messenger $client, LoopInterface $loop)
+    {
+        foreach ($this->messages as $message) {
+            $client->on($message->event, function($arg) use ($message, $loop) {
+                $this->assertEquals($arg, $message->arguments);
+                $message->called = true;
+                $this->totalCalled++;
+
+                if ($this->totalCalled === count($this->messages)) {
+                    $loop->stop();
+                }
+            });
+
+            $loop->addTimer(0.001, function() use ($server, $message) {
+                $server->send($message->event, $message->arguments);
+            });
+        }
+
+        $loop->addTimer(3, function() use ($loop) {
+            $loop->stop();
+
+            throw new \RuntimeException('Timeout reached, server has not accomplished all messages.');
+        });
+
+        $loop->run();
+
+        foreach ($this->messages as $message) {
+            $this->assertTrue($message->called);
+        }
+    }
+}
+
+class TestMessage
+{
+    public $event;
+    public $arguments;
+    public $called = false;
+
+    public function __construct($event, $arguments)
+    {
+        $this->event = $event;
+        $this->arguments = $arguments;
     }
 }
